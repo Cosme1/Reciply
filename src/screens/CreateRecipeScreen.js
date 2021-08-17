9b20c6fa27ee838713f821b4a8ce1e7ad6fdd94d
Cosme1 <cosme.schuenemsnn@gmail.com>
2021-08-17 09:08:35 +0200

    this component doesn't work

--- src/screens/CreateRecipeScreen.js
+++ src/screens/CreateRecipeScreen.js
@@ -12,17 +12,68 @@ import {
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import * as ImagePicker from 'react-native-image-picker';
 
 import {Food} from './RecipesScreen';
+import { useState } from 'react/cjs/react.production.min';
 
 let Pasta2 = new Food();
 
+
+
 function CreateRecipeLayout() {
+	const setImageSource = useState<String|undefined>('../assets/Placeholder.png');
+	var image = Boolean(false);
+	renderImage = () => {
+		if (setImageSource) {
+			console.log('active');
+			return <Image
+			  source={{uri: setImageSource }}
+			  style={styles.image}
+			/>
+		  } else {
+			  console.log('inactive')
+			return <Image
+			  source={require('../assets/Placeholder.png')}
+			  style={styles.image}
+			/>
+		  }
+	}
   const text = React.useState(null);
   return (
     <ScrollView>
-      <Button title="Photos" onPress={this.Photos}></Button>
-	  <Button title="Navigate" onPress={this.navigatePhotos}></Button>
+      <Button
+        onPress={() =>
+          ImagePicker.launchImageLibrary(
+            {
+              mediaType: 'photo',
+              includeBase64: false,
+              maxHeight: 200,
+              maxWidth: 200,
+            },
+            response => {
+				console.log('Response = ', response);
+              if (response.didCancel) {
+                console.log('canceled');
+              } else {
+                setImageSource(response.uri);
+				image = true
+				console.log(response);
+
+				this.setState({
+					fileUri: response.uri,
+			  });
+              }
+			  
+              
+            },
+          )
+        }
+        title="Select Image"
+      />
+	<TouchableOpacity>
+		{renderImage()}
+	</TouchableOpacity>
       <TextInput style={styles.input} value={text} placeholder="Name" />
     </ScrollView>
   );
