0a5fff53f5492d63c72791b0a19fdb0cd267247f
Cosme1 <cosme.schuenemsnn@gmail.com>
2021-08-17 16:23:51 +0200

    Image works now

--- android/app/src/main/java/com/reciply/MainApplication.java
+++ android/app/src/main/java/com/reciply/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.imagepicker.ImagePickerPackage;
--- src/screens/CreateRecipeScreen.js
+++ src/screens/CreateRecipeScreen.js
@@ -1,79 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Button,
   View,
   Text,
   ScrollView,
   TouchableOpacity,
-  Image,
   StyleSheet,
   TextInput,
+  ImageProps,
 } from 'react-native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import * as ImagePicker from 'react-native-image-picker';
-
+import ImagePicker from 'react-native-image-crop-picker';
+import {Image} from 'react-native-elements';
 import {Food} from './RecipesScreen';
-import { useState } from 'react/cjs/react.production.min';
+import ImageCropPicker from 'react-native-image-crop-picker';
 
 let Pasta2 = new Food();
 
-
-
 function CreateRecipeLayout() {
-	const setImageSource = useState<String|undefined>('../assets/Placeholder.png');
-	var image = Boolean(false);
-	renderImage = () => {
-		if (setImageSource) {
-			console.log('active');
-			return <Image
-			  source={{uri: setImageSource }}
-			  style={styles.image}
-			/>
-		  } else {
-			  console.log('inactive')
-			return <Image
-			  source={require('../assets/Placeholder.png')}
-			  style={styles.image}
-			/>
-		  }
-	}
+  const [image, setImage] = useState(undefined);
+  const choosePhotofromLibrary = () => {
+    ImageCropPicker.openPicker({
+      width: 700,
+      height: 400,
+      cropping: true,
+    }).then(image => {
+      console.log(image);
+      setImage(image.path);
+    });
+  };
+
   const text = React.useState(null);
   return (
     <ScrollView>
-      <Button
-        onPress={() =>
-          ImagePicker.launchImageLibrary(
-            {
-              mediaType: 'photo',
-              includeBase64: false,
-              maxHeight: 200,
-              maxWidth: 200,
-            },
-            response => {
-				console.log('Response = ', response);
-              if (response.didCancel) {
-                console.log('canceled');
-              } else {
-                setImageSource(response.uri);
-				image = true
-				console.log(response);
-
-				this.setState({
-					fileUri: response.uri,
-			  });
-              }
-			  
-              
-            },
-          )
+      <Image
+        source={{uri: image}}
+        style={styles.image}
+        PlaceholderContent={
+          <Image source={require('../assets/Placeholder.png')} />
         }
-        title="Select Image"
       />
-	<TouchableOpacity>
-		{renderImage()}
-	</TouchableOpacity>
+      <Button title={'Choose Photo'} onPress={choosePhotofromLibrary}></Button>
       <TextInput style={styles.input} value={text} placeholder="Name" />
     </ScrollView>
   );
@@ -119,8 +88,10 @@ const styles = StyleSheet.create({
   },
 
   image: {
-    height: 100,
     padding: 10,
+	margin: 10,
+	minHeight: 200,
+	resizeMode: 'contain',
   },
 });
 
